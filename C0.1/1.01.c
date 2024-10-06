3e209d5f251f30ad67fed320acff625bfcdfe553
Maxime Crdvlt <maxime.courdavault@etu.u-paris.fr>
2024-10-06 18:19:21 +0200

    C0 - C1 - C2 ✔️

--- C0.1/1.01.c
+++ C0.1/1.01.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #pragma warning(disable : 4996 6031)
 
-enum { MAX_ETUDIANTS = 100, NOM_MAX = 31, MAX_CARACTERES_ABSENCES = 50, INPUTMAX = 100 };
-
-typedef struct {
-	int id, jour;
-	char dj[2];
-} Absence;
+enum { MAX_ETUDIANTS = 100, NOM_MAX = 31, INPUTMAX = 100 };
+enum { MAX_CARACTERES_ABSENCES = 50, MAX_ABSENCES = 100, MAX_CARACTERES_DMJ = 3, MAX_JOURS = 40 };
 
 typedef struct {
 	char nom[NOM_MAX];
 	int groupe;
-	Absence absences[];
+	int id;
 } Etudiant;
 
+typedef struct {
+	int id;
+	int jour;
+	char dj[MAX_CARACTERES_DMJ];
+	char etat[100];
+} Absence;
 
+// Enregistrement des étudiants
 Etudiant etudiants[MAX_ETUDIANTS];
-Absence absences[];
 int compteur_etudiants = 1;
 
+//Enregistrement des absences
+Absence absences[MAX_ABSENCES];
+int compteur_absences = 1;
+
 void inscription(char nom[NOM_MAX], int groupe) {
+	//Vérifications
+	int verifnom = strlen(nom);
+	if  (verifnom> NOM_MAX-1)
+	{
+		printf("Nom incorrect \n");
+		return;
+	}
 	//Vérification de l'unicité
 	for (int i = 0; i < compteur_etudiants; ++i) 
 	{
@@ -32,7 +44,7 @@ void inscription(char nom[NOM_MAX], int groupe) {
 		}
 	}
 
-	//Inscription de l'étudiant
+	//Enregistrement de l'étudiant
 	if (compteur_etudiants < MAX_ETUDIANTS) {
 		strcpy(etudiants[compteur_etudiants].nom, nom);
 		etudiants[compteur_etudiants].groupe = groupe;
@@ -45,24 +57,45 @@ void inscription(char nom[NOM_MAX], int groupe) {
 	}
 }
 
-void absence(int id, int jour, char dj[2])
+void absence(int id, int jour, char dj[MAX_CARACTERES_DMJ])
 {
 	//Vérifications
 	if (id>compteur_etudiants)
 	{
-		printf("Identifiant incorrect");
+		printf("Identifiant incorrect\n");
+		return;
 	}
-	else if (jour < 0 || jour>40)
+	if (jour <= 0 || jour > 40)
 	{
-		printf("Date incorrecte");
+		printf("Date incorrecte\n");
+		return;
 	}
-	else if (strcmp(dj[], "am")!=0 || strcmp(dj[], "pm")!=0)
+	if (strcmp(dj, "am")!=0 && strcmp(dj, "pm")!=0)
 	{
-		printf("Demi-journee incorrecte");
+		printf("Demi-journee incorrecte\n");
+		return;
 	}
-	for (int i = 0; i<compteur_etudiants; ++i)
+	//Vérification de l'unicité
+	for (int i = 0; i < compteur_absences; i++) 
 	{
-		if (strcmp())
+		if (strcmp(absences[i].dj, dj) == 0 && absences[i].jour == jour)
+		{
+			printf("Absence deja connue\n");
+			return;
+		}
+	}
+
+	//Enregistrement de l'absence
+	if (compteur_etudiants < MAX_ETUDIANTS) {
+		absences[compteur_absences].id = id;
+		absences[compteur_absences].jour = jour;
+		strcpy(absences[compteur_absences].dj, dj);
+		printf("Absence enregistree (%d)\n", compteur_absences);
+		printf("%d %d %s \n", absences[compteur_absences].id, absences[compteur_absences].jour, absences[compteur_absences].dj);
+		compteur_absences++;
+	}
+	else {
+		printf("Nombre maximum d'absences atteint...\n");
 	}
 }
 
@@ -71,7 +104,7 @@ int main() {
 	char nom[NOM_MAX];
 	int groupe;
 	int id, jour;
-	char dj[2];
+	char dj[MAX_CARACTERES_DMJ];
 	do
 	{
 		scanf("%s", commande);
@@ -86,10 +119,14 @@ int main() {
 		//Absence - C2
 		if (strcmp(commande, "absence") == 0)
 		{
-			scanf("%d %s", &id, &jour, dj);
+			scanf("%d %d %s", &id, &jour, dj);
 			absence(id, jour, dj);
 		}
 
+		else
+		{
+			printf("Commande invalide...\n");
+		}
 	} while (strcmp(commande, "exit") != 0);
 
 	printf("\n");
